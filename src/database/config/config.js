5c40d928ed2a546c56e2bd507199b38caaf48c28
Nailton Junior <51039019+Naillton@users.noreply.github.com>
2023-02-02 16:13:08 -0300

    Update config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const config = {
   username: process.env.MYSQL_USER || 'root',
   port: process.env.MYSQL_PORT || '3306',
-  password: process.env.MYSQL_PASSWORD || '89@junior',
+  password: process.env.MYSQL_PASSWORD || '',
   database: process.env.MYSQL_DATABASE || 'login',
   host: process.env.MYSQL_HOST || 'localhost',
   dialect: 'mysql',
